dc2874ecfd963d68bb921fcfc994f8fee39b8ff3
Logon Aluno <logonaluno@local.com>
2019-11-25 21:47:10 -0300

    adicionando metodos

--- filme/src/main/java/com/netflix/filme/controller/FilmeController.java
+++ filme/src/main/java/com/netflix/filme/controller/FilmeController.java
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-package com.netflix.filme.controller;
-
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import com.netflix.filme.producer.FilmeProducer;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-@RestController
-@RequestMapping(value = "/ordemfilme")
-@Slf4j
-public class FilmeController {
-
-	private final FilmeProducer filmeProducer;
-	
-	private static final Logger logger = LogManager.getLogger(FilmeController.class);
-
-	private FilmeController(FilmeProducer filmeProducer) {
-		this.filmeProducer = filmeProducer;
-	}
-
-	@RequestMapping(method = RequestMethod.POST)
-	@CacheEvict
-	public void send(@RequestBody String ordemFilme) {
-		filmeProducer.enviar(ordemFilme);
-		logger.info("Mensagem do filme postada !");
-	}
-
-}
-=======
 package com.netflix.filme.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -83,4 +48,3 @@ public class FilmeController {
 	}
 
 }
->>>>>>> branch 'master' of https://github.com/BrunoCarvalho18/ms-netflix.git
