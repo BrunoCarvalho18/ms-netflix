c443ba9cc43816d7f5c238c3d667959aabd834b4
Logon Aluno <logonaluno@local.com>
2019-11-25 21:42:22 -0300

    adicionando metodos

--- filme/src/main/java/com/netflix/filme/controller/FilmeController.java
+++ filme/src/main/java/com/netflix/filme/controller/FilmeController.java
@@ -2,6 +2,8 @@ package com.netflix.filme.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,6 +25,16 @@ public class FilmeController {
 	@GetMapping("/assistir/{idFilme}/{idUsuario}")
 	public void assistirFilme (@PathVariable Long idFilme, @PathVariable Long idUsuario) {
 		send("Filme "+idFilme+" assistido pelo usuario "+idUsuario);
+	}
+	
+	@GetMapping("/enfileirar/{idFilme}/{idUsuario}")
+	public void enfileirarFilme (@PathVariable Long idFilme, @PathVariable Long idUsuario) {
+		send("Filme "+idFilme+" enfileirado pelo usuario "+idUsuario);
+	}
+	
+	@PutMapping("/classificar/{idFilme}/{classificacao}/{idUsuario}")
+	public void classificarFilme (@PathVariable Long idFilme, @PathVariable int classificacao, @PathVariable Long idUsuario) {
+		//TODO
 	}
 
 	private FilmeController(FilmeProducer filmeProducer) {
